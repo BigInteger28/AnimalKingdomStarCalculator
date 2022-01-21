2dfe743239ec834075ff97174099f01d61ed2f3d
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2022-01-21 18:47:24 +0100

    tabs

--- main.c
+++ main.c
@@ -12,42 +12,42 @@ int main()
 {
 	while (1 != 2)
     {
-	float multiplier;
-	float ownRating;
-	float sumRatingAllOpponents;
-	float matches;
-	float score;
-	float averageOpponent;
-	float highestRatingInList; //Voorlopig is dit Amon
-	char placements;
-	char human;
-	char temp;
+		float multiplier;
+		float ownRating;
+		float sumRatingAllOpponents;
+		float matches;
+		float score;
+		float averageOpponent;
+		float highestRatingInList; //Voorlopig is dit Amon
+		char placements;
+		char human;
+		char temp;
 		
         printf("Placements? (y): ");
         scanf("%c", &placements);
         printf("Total sum all ratings played against: ");
-	scanf("%c", &temp);
-	if (temp == '.') {
-		printf("Average rating opponents: \n");
-		scanf("%f", &averageOpponent);
-	} else {
-		sumRatingAllOpponents = (float)temp;
-	}
-	printf("Matches played: \n");
-	scanf("%f", &matches);
-	printf("Score: \n");
-	scanf("%f", &score);
-	if (placements == 'y') {			
+		scanf("%c", &temp);
+		if (temp == '.') {
+			printf("Average rating opponents: \n");
+			scanf("%f", &averageOpponent);
+		} else {
+			sumRatingAllOpponents = (float)temp;
+		}
+		printf("Matches played: \n");
+		scanf("%f", &matches);
+		printf("Score: \n");
+		scanf("%f", &score);
+		if (placements == 'y') {			
             multiplier = 8;
             ownRating = (sumRatingAllOpponents / matches);			
-	}
-	else {
-		printf("Multiplier: \n");
-		scanf("%f", &multiplier);
-    		printf("Your own rating: \n");
-    		scanf("%f", &ownRating);
-	}
-	if (temp != '.') { averageOpponent = sumRatingAllOpponents / matches; }
+		}
+		else {
+			printf("Multiplier: \n");
+			scanf("%f", &multiplier);
+            printf("Your own rating: \n");
+            scanf("%f", &ownRating);
+		}
+		if (temp != '.') { averageOpponent = sumRatingAllOpponents / matches; }
 		printf("\nAverage opponent is %.1f", averageOpponent);
 		printf("\nYour new rating is %.1f", newRating(multiplier, ownRating, averageOpponent, matches, score));
 		printf("\n\n");
